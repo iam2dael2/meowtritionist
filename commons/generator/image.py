b8efda11bb59e9382bbf6d7c9a92d6835f0cfeb1
Darell Hermawan <iam2dael2@gmail.com>
2025-03-12 13:33:04 +0700

    use google instead of chromium

--- commons/generator/image.py
+++ commons/generator/image.py
@@ -19,7 +19,7 @@ def init_chromedriver():
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
 
-    service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(options=chrome_options, service=service)
     
     return driver
